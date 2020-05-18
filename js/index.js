3fe37c54ba01dde1577bfbe9dd00be1875504f59
consolelogreece <consolelogreece@gmail.com>
2020-05-18 12:23:53 +0100

    refactor

--- js/index.js
+++ js/index.js
@@ -1,3 +1,43 @@
+let gameCanvas = document.getElementById("GameCanvas");
+
+let ctx = gameCanvas.getContext("2d");
+
 document.addEventListener("DOMContentLoaded", function() {
-    InitGame();
+    Setup();
 });
+
+let Stack = {};
+
+let state = "Game";
+
+let infoBarHeight = 40; //px
+
+function InitGame()
+{
+    game = new Game(2170, 12, infoBarHeight, ctx);
+
+    gameCanvas.onclick = e => game.HandleClick(e);
+
+    Stack.Game = game;
+}
+
+function Setup()
+{
+    InitGame();
+
+    mainLoop();
+}
+
+function HandleClick(e)
+{
+    Stack[state].HandleClick(e);
+}
+
+function mainLoop(DOMHRTS)
+{
+    Stack[state].Update();
+    
+    Stack[state].Draw();
+ 
+    requestAnimationFrame(_ => mainLoop(_));
+}
